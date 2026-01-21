036b3c6ff934a41835fd7ad45b9ea35117a5f58e
sarah-larkin <larkinasarah@gmail.com>
2026-01-21 17:11:44 +0000

    updated obfuscation service section to include s3 client code

--- services/obfuscation_service.py
+++ services/obfuscation_service.py
@@ -1,4 +1,5 @@
 from input.validators import ObfuscationRequest
+from infrastructure.s3_client import S3Client
 import time
 
 #orchestration
@@ -7,13 +8,21 @@ import time
 
 #creating the structure
 class ObfuscationService:
+    def __init__(self, s3_client: S3Client): 
+        self.s3_client = s3_client #calling in from infra/s3_client.py
+
     def run(self, request: ObfuscationRequest):
         #get file using request.file_details
-        file = self.load_file(request.file_details) # TODO write load_file code 
+        bucket = request.file_details["Bucket"]
+        key = request.file_details["Key"]
+        data_bytes = self.s3_client.download_file(bucket, key) 
+        
         #convert to df
-        df = self.parse(file) # TODO: write parse code 
+        df = self.parse(request.file_details, data_bytes) # TODO: write parse code 
+        
         #obfuscate using request.fields
-        df = self.obfuscate(df, request.fields) # TODO: write obfuscate code
+        obf_df = self.obfuscate(df, request.fields) # TODO: write obfuscate code
+        
         #output 
         self.write_output(df) # TODO: write write_output code
 
